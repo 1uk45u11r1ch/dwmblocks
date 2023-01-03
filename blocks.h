83fb5f3a19691bfa467900d78f742574ca55dcbe
1337h4x0r <1337h4x0r@gmx.de>
2023-01-03 21:43:12 +0100

    move time block to normal statusbar

--- blocks.h
+++ blocks.h
@@ -1,13 +1,13 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	
+
+	{"[ TME: ",	"echo \"$(date '+%k:%M %d.%m.%Y')\" ]",							1,		0},
 	{"[ CPU: ",	"echo \"$(cat /run/cpustatsd/load)%\" ]",						1,		0},
 	{"[ CLK: ",	"echo \"$(cat /run/cpustatsd/maxclk)MHz | $(cat /run/cpustatsd/minclk)MHz\" ]",		1,		0},
 	{"[ MEM: ",	"echo $(free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g) ]",			1,		0},
 	{";",		"",											86400,		0}, /* dwm-extrabar delimeter */
 	{";",		"",											86400,		0}, /* dwm-extrabar delimeter */
-	{"",		"date '+%k:%M %d.%m.%Y'",								1,		0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
